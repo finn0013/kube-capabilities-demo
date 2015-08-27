77b7a5feee003a64f05e8ed3428527c4ddc084c6
Jason McDonald <finn0013@gmail.com>
2015-08-27 19:06:10 -0400

    Adding additional helpful 'commands'.

--- kubernetes/kubernetes_helpers/kube-commands.sh
+++ kubernetes/kubernetes_helpers/kube-commands.sh
@@ -1,5 +1,5 @@
 echo -e "\033[1;32m"
-echo -e "kubectl get [nodes | pods | services | replication controllers]\033[0m"
+echo -e "kubectl get [nodes | pods | services | replication controllers | namespaces | secrets | endpoints <pod_name>]\033[0m"
 echo Lists all nodes, pods, services, or replication controllers, respectively.
 
 echo -e "\033[1;32m"
@@ -11,8 +11,21 @@ echo -e "kubectl logs <pod_name>\033[0m"
 echo Lists most recent logs for a given pod. Often enough for basic debugging.
 
 echo -e "\033[1;32m"
-echo -e "kubectl get endpoints <pod_name>\033[0m"
+echo -e "kubectl get config view\033[0m"
 echo "List the IP address endpoints for the pod(s)."
+
+echo -e "\033[1;32m"
+echo -e "kubectl cluster-info\033[0m"
+echo "List the IP address endpoints for the pod(s)."
+
+echo -e "\033[1;32m"
+echo -e "kubectl create -f [file] --validate\033[0m"
+echo Validate the correctness of a configuration file.
+
+echo -e "\033[1;32m"
+echo -e kubectl create -f cluster/addons/kube-ui/kube-ui-rc.yaml --namespace=kube-system
+echo -e "kubectl create -f cluster/addons/kube-ui/kube-ui-svc.yaml --namespace=kube-system\033[0m"
+echo Start Kubernetes UI. Get the stood up Url with ‘kubectl cluster-info’
 
 echo
 echo
