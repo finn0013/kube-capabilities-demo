ae50c51903099f8f387b90e79d7eff9d4e4113a6
Jason McDonald <finn0013@gmail.com>
2015-08-27 16:28:34 -0400

    Updated to use kubectl instead of of kubectl.sh. Was getting odd error with the sh script (bash: KUBE_VERSION_REGEX: readonly variable).

--- kubernetes/kubernetes_helpers/kube-all.sh
+++ kubernetes/kubernetes_helpers/kube-all.sh
@@ -4,25 +4,25 @@ echo
 echo -e "\033[1;32m"
 echo NODES:
 echo -e "========================\033[0m"
-. kubectl.sh get nodes
+kubectl get nodes
 echo
 
 echo -e "\033[1;32m"
 echo PODS:
 echo -e "========================\033[0m"
-. kubectl.sh get pods
+kubectl get pods
 echo
 
 echo -e "\033[1;32m"
 echo SERVICES:
 echo -e "========================\033[0m"
-. kubectl.sh get services
+kubectl get services
 echo
 
 echo -e "\033[1;32m"
 echo REPLICATION CONTROLLERS:
 echo -e "========================\033[0m"
-. kubectl.sh get replicationcontrollers
+kubectl get replicationcontrollers
 echo
 
 echo
