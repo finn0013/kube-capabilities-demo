4724ce5e3d9bbba6f106dd859d068d4602ea7f9c
Jason McDonald <finn0013@gmail.com>
2015-09-08 14:16:24 -0400

    Added -f option description to kubectl logs ... command help.

--- kubernetes/kubernetes_helpers/kube-help.sh
+++ kubernetes/kubernetes_helpers/kube-help.sh
@@ -8,7 +8,7 @@ echo Lists basic information and events for a given pod.
 
 echo -e "\033[1;32m"
 echo -e "kubectl logs <pod_name>\033[0m"
-echo Lists most recent logs for a given pod. Often enough for basic debugging.
+echo Lists most recent logs for a given pod. Often enough for basic debugging. Use -f to update in real time.
 
 echo -e "\033[1;32m"
 echo -e "kubectl config view\033[0m"
